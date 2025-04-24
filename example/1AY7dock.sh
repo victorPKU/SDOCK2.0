194323d40335abdef556430d0d1504da28bfffb3
victorPKU <93566301+victorPKU@users.noreply.github.com>
2025-04-24 11:39:36 +0800

    Add files via upload

--- example/1AY7dock.sh
+++ example/1AY7dock.sh
@@ -5,7 +5,7 @@
 ../bin/watmap 1AY7/1AY7R.pdb 1AY7/1AY7Rwat.pdb -a ../data/ATM
 ../bin/watmap 1AY7/1AY7L.pdb 1AY7/1AY7Lwat.pdb -a ../data/ATM
 ../bin/sdock 1AY7/1AY7R.pdb 1AY7/1AY7L.pdb -o 1AY7/1AY7_record -r ../data/so3layer.qua
-../bin/build 1AY7/1AY7R.pdb 1AY7/1AY7L.pdb -o 1AY7/1AY7_record -r ../data/so3layer.qua -c 66 -m 1 -n 1AY7 -d 1AY7/model/
+../bin/build 1AY7/1AY7R.pdb 1AY7/1AY7L.pdb -o 1AY7/1AY7_record -r ../data/so3layer.qua -c 59 -m 1 -n 1AY7 -d 1AY7/model/
 ../bin/sdock 1AY7/1AY7R.pdb 1AY7/1AY7L.pdb 1AY7/1AY7Rwat.pdb 1AY7/1AY7Lwat.pdb -o 1AY7/1AY7_watrecord -r ../data/so3layer.qua
-../bin/build 1AY7/1AY7R.pdb 1AY7/1AY7L.pdb -o 1AY7/1AY7_watrecord -r ../data/so3layer.qua -c 66 -m 1 -n 1ay7 -d 1AY7/model/
+../bin/build 1AY7/1AY7R.pdb 1AY7/1AY7L.pdb -o 1AY7/1AY7_watrecord -r ../data/so3layer.qua -c 2 -m 1 -n 1ay7 -d 1AY7/model/
 
